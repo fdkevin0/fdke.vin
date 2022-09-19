f73a1b6430982ea65b7c450394af8ba13f886d6d
FDKevin <fdkevinzero@gmail.com>
2022-09-20 01:18:58 +0800

    some updates

--- src/site-meta.config.ts
+++ src/site-meta.config.ts
@@ -1,9 +1,9 @@
 const siteMeta = {
-  title: "Astro Theme Cactus",
-  description: "An opinionated starter theme for Astro",
+  title: "FDKevin's personal website",
+  description: "FDKevin's personal website",
   lang: "en-GB",
   ogLocale: "en_GB",
-  githubUrl: "https://github.com/chrismwilliams/astro-theme-cactus",
+  githubUrl: "https://github.com/fdkevin0",
   themeColorLight: "#fafafa",
   themeColorDark: "#1d1f21",
 }
