3bcc1cdfd84c3773a9a4c40586eb6db636a28fac
fdkevin <25277753+fdkevin0@users.noreply.github.com>
2026-03-19 20:13:16 +0000

    fix: accept date type for note collection publishDate

--- src/content.config.ts
+++ src/content.config.ts
@@ -44,6 +44,7 @@ const note = defineCollection({
 		publishDate: z
 			.string()
 			.datetime({ offset: true }) // Ensures ISO 8601 format with offsets allowed (e.g. "2024-01-01T00:00:00Z" and "2024-01-01T00:00:00+02:00")
+			.or(z.date())
 			.transform((val) => new Date(val)),
 	}),
 });
